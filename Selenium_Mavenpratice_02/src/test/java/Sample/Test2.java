e936b425cd44425a709fc56e5d4627c52342a7aa
Sandeepramya <sandeepramya01062023@outlook.com>
2025-11-12 10:53:33 +0530

    Test2 testcase modified

--- Selenium_Mavenpratice_02/src/test/java/Sample/Test2.java
+++ Selenium_Mavenpratice_02/src/test/java/Sample/Test2.java
@@ -6,6 +6,21 @@ public class Test2 {
 	
 	@Test
 	public void CreateOrgTest() {
+		
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");//if we are not passing maven parameters default value also used
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+//		String URL=System.getProperty("url","http://localhost:8888");
+//		String BROWSER=System.getProperty("browser","chrome");//if we are not passing maven parameters default value also used
+//		String USERNAME=System.getProperty("username","admin");
+//		String PASSWORD=System.getProperty("password","admin");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+	    System.out.println(USERNAME);
+	    System.out.println(PASSWORD);
 		System.out.println("excute CreateOrg & verify");
 	}
 	
